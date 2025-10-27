67e6278f7632603d5b9dbe51ea0bea789c381ea1
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-10-27 14:28:54 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int n1 = Integer.parseInt(System.console().readLine("Introduzca un número: "));
+
+        System.out.println("  n |  n2  |  n3");
+        System.out.println("----------------");
+        
+        for (int i = n1; i < n1 + 5; i++){
+            System.out.printf("%5d |%8d |%9d\n", i, i*i, i*i*i);
+        }
     }
 }
